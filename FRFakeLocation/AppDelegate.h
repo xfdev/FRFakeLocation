3a1b5593883808e36c7552c8504155dd32deadb6
sonny <anangcocoy@gmail.com>
2017-01-23 16:58:02 +0800

    init。。。。。

---
+++ FRFakeLocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FRFakeLocation
+//
+//  Created by sonny on 17/1/23.
+//  Copyright © 2017年 xfdev.github.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FRFakeLocation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FRFakeLocation
+//
+//  Created by sonny on 17/1/23.
+//  Copyright © 2017年 xfdev.github.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
